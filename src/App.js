12869c534277ee711720b7e86f2558550a544c11
suganthsugi <suganthjayanthi@gmail.com>
2023-02-28 15:22:18 +0530

    hooks - Context, Reduser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import React, {useReducer} from 'react';
 import './App.css';
 import A from './components/A';
 import C from './components/C';
 import B from './components/D';
 
+export const usercon=React.createContext()
 function App() {
+  const ini=0;
+  
+  const reducer = (state, action)=>{
+    if(action.choice==="inc"){
+      return state+1
+    }
+    if(action.choice==="dec"){
+      return state-1
+    }
+    if(action.choice==="reset"){
+      return 0
+    }
+  }
+  
+  const [count, operate] = useReducer(reducer, ini)
+
   return (
     <div className="App">
-      <h1>Count : 0</h1>
+      <h1>Count : {count}</h1>
+      <usercon.Provider value={operate}>
       <A />
       <br />
       <B />
       <br />
       <C />
+    </usercon.Provider>
     </div>
   );
 }
--- src/components/A.js
+++ src/components/A.js
@@ -1,12 +1,13 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { usercon } from "../App";
 function A(){
+    const dispatch=useContext(usercon)
     return(
         <div>
             A : 
-            <button>Inc</button>
-            <button>Dec</button>
-            <button>Reset</button>
+            <button onClick={()=>dispatch({choice:"inc"})}>Inc</button>
+            <button onClick={()=>dispatch({choice:"dec"})}>Dec</button>
+            <button onClick={()=>dispatch({choice:"reset"})}>Reset</button>
         </div>
     )
 }
